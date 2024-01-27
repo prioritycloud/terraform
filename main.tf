2754fdf8a78e268a0b37a2892c235ec5a9e410df
prioritycloud <153988091+prioritycloud@users.noreply.github.com>
2024-01-27 18:58:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,15 @@
+# main.tf
+
+provider "aws" {
+  region = "ap-south-1"  # Modify this region as needed
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0aee0743bf2e81172"  # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Jenkins-EC2-Instance"
+  }
+}
 
